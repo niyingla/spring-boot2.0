10bf1456a915fef327a1a14fb8f78bebacd746f8
微笑只是问候 <1047984367@qq.com>
2020-05-11 00:10:37 +0800

    初始化

--- spring-application/src/main/java/com/imooc/diveinspringboot/SpringApplicationBootstrap.java
+++ spring-application/src/main/java/com/imooc/diveinspringboot/SpringApplicationBootstrap.java
@@ -11,6 +11,11 @@ import java.util.Set;
 /**
  * {@link SpringApplication} 引导类
  *
+ * 通过堆栈推倒启动mian方法 deduceMainApplicationClass
+ * new RuntimeException()).getStackTrace() 可以获取堆栈 堆栈可以拿到外层调用方法
+ *
+ *
+ * deduceEnvironmentClass 判断环境类型 SERVLET、REACTIVE、无
  * @author 小马哥
  * @since 2018/5/16
  */
@@ -23,11 +28,20 @@ public class SpringApplicationBootstrap {
         // 配置Class 名称
         sources.add(ApplicationConfiguration.class.getName());
         SpringApplication springApplication = new SpringApplication();
+        // 方法里添加参数来配置源
+        // 也可以自定义java配置，可以看出，传入的class，
+        // 不一定是运行类的class，也可以是自己定义的class，
+        // 只要有SpringBootApplication注解即可。
         springApplication.setSources(sources);
+        //手动设置应用类型
+//        springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
 
     }
 
+    /**
+     * 注解所在对象也会被管理成bean
+     */
     @SpringBootApplication
     public static class ApplicationConfiguration {
 
